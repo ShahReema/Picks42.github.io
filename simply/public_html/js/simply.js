f6189207fa28bbb715df8a2dbc5296b253c5c217
lucentinnovation <lucentinnovation@lucentinnovation>
2016-07-27 18:31:58 +0530

    working on drop-downs menu completed, tables-completed,buttons-completed  -Prakash Prabhakar

--- simply/public_html/js/simply.js
+++ simply/public_html/js/simply.js
@@ -7,32 +7,33 @@
 
 $(document).ready(function () {
     //add dropdown class to the drop down li
-
+    window.current_window_size = $(window).width();
     function dropdown_class_hover() {
-        var select = $(".main_ul .drop_down_hover");
+        var select = $(".drop_down_hover");
         var icon = "<i class='fa fa-caret-down' aria-hidden='true'></i>";
         select.parent().addClass("has_dropdown hover_effect");
         $(".hover_effect>a").append(icon);
     }
     function dropdown_class_click() {
-        var select = $(".main_ul .drop_down_click");
+        var select = $(".drop_down_click");
         var icon = "<i class='fa fa-caret-down' aria-hidden='true'></i>";
         select.parent().addClass("has_dropdown click_effect");
         $(".click_effect>a").append(icon);
-
     }
     dropdown_class_hover();
     dropdown_class_click();
-
-    $(".main_ul .has_dropdown.hover_effect").hover(function () {
-        $(this).find(".drop_down_hover").stop().fadeIn();
+    $(".has_dropdown.hover_effect").hover(function () {
+        if (window.current_window_size > 1025)
+        {
+            $(this).find(".drop_down_hover").stop().fadeIn();
+        }
     },
             function () {
-                $(this).find(".drop_down_hover").stop().fadeOut();
+                if (window.current_window_size > 1025) {
+                    $(this).find(".drop_down_hover").stop().fadeOut();
+                }
             });
-
-    $(".main_ul .has_dropdown.click_effect").click(function () {
-
+    $(document).on('click', ".has_dropdown.click_effect", function () {
         $(this).find(".drop_down_click").toggle();
     });
     //create close button
@@ -47,5 +48,30 @@ $(document).ready(function () {
     $(".menu_wrapper .close_btn").click(function () {
         $(".menu_wrapper .main_menu").slideToggle();
     });
+    $(".hover_effect").attr("data-hover", "true");
 
+    //disable hover in touch screen
+    function disable_hover() {
+
+        var select = $(".has_dropdown[data-hover='true']");
+        var ul = $(".has_dropdown[data-hover='true']>ul");
+        var window_width = $(window).width();
+        if (window_width <= 1024) {
+            select.removeClass("hover_effect").addClass("click_effect");
+            ul.removeClass("drop_down_hover").addClass("drop_down_click");
+        } else {
+            select.addClass("hover_effect").removeClass("click_effect");
+            ul.addClass("drop_down_hover").removeClass("drop_down_click");
+        }
+    }
+    disable_hover();
+    
+    //dropdowns
+    $(".simple-drop_down").attr("data-effect","click");
+    $(".simple_drop_down.style_hover").attr("data-effect","hover");
+//window resize functions
+    $(window).resize(function () {
+        window.current_window_size = $(window).width();
+        disable_hover();
+    });
 });
